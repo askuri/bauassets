a9ffed1614e96f1752ea337bf1408da1f12cb1cc
askuri <enwi2@t-online.de>
2020-02-09 13:08:11 +0100

    Fix that tests randomly fail
    The faker classes may generate names containing ' characters.
    Laravel's assertSee() can't tell ' and its encoded variant are the same
    thing, so the tests failed every time there was a ' in the test data.

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -9,7 +9,7 @@ $factory->define(Loan::class, function (Faker $faker) {
     return [
         'date_given' => now(),
         'date_returned' => now(),
-        'borrower_name' => $faker->name,
+        'borrower_name' => $faker->text(20),
         'borrower_room' => 104, // LoanTests, testUpdateLoanAuthorized increases this by one!
         'comment' => 'kjahsdfk',
         'borrower_email' => "e@e",
