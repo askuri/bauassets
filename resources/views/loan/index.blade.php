1e4d015591e15bf86d82f8d8c9f74fbd0de055e2
askuri <enwi2@t-online.de>
2020-02-14 14:01:18 +0100

    Refactor: clickable table rows script now external
    This script is useful for other pages also, so it is moved into
    its own file.

--- resources/views/loan/index.blade.php
+++ resources/views/loan/index.blade.php
@@ -31,23 +31,4 @@
     </tbody>
 </table>
 
-<script>
-    /*
-     * Make whole row clickable by adding data-href attribute to it
-     */
-$(function(){
-    $('.table tr[data-href]').each(function(){
-        $(this).css('cursor','pointer').hover(
-            function(){ 
-                $(this).addClass('active'); 
-            },  
-            function(){ 
-                $(this).removeClass('active'); 
-            }).click( function(){ 
-                document.location = $(this).attr('data-href'); 
-            }
-        );
-    });
-});
-</script>
 @endsection
